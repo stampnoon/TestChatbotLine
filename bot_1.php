cffcff914cab738284d2e6e1c923b55ef4be1d4d
stampnoon <thnong_stamp@hotmail.com>
2020-03-19 16:35:41 +0700

    asd

--- bot_1.php
+++ bot_1.php
@@ -64,12 +64,15 @@ $content = file_get_contents('php://input');
 // แปลงข้อความรูปแบบ JSON  ให้อยู่ในโครงสร้างตัวแปร array
 $events = json_decode($content, true);
 if (!is_null($events)) {
+
+    $token = strval($content);
+
     // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
     $replyToken = $events['events'][0]['replyToken'];
     $typeMessage = $events['events'][0]['message']['type'];
     $userMessage = $events['events'][0]['message']['text'];
     $userMessage = strtolower($userMessage);
-    $token = strval($replyToken);
+
     switch ($typeMessage) {
         case 'text':
             switch ($userMessage) {
